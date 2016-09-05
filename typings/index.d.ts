d9bb2368473735db859d61348d2534076ae95932
Erik Vullings <erik.vullings@gmail.com>
2016-09-05 14:53:07 +0200

    Added typings for websockets

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -4,3 +4,4 @@
 /// <reference path="globals/restler/index.d.ts" />
 /// <reference path="globals/turf/index.d.ts" />
 /// <reference path="globals/winston/index.d.ts" />
+/// <reference path="globals/ws/index.d.ts" />
